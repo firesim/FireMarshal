4b4140542c697d28a15695a09bb52636dc70d5fa
Nathan Pemberton <nathanp@berkeley.edu>
2017-11-01 15:18:28 -0700

    cleaning up tests

--- pfa_tests/qsort/qsort.mk
+++
@@ -1,6 +0,0 @@
-QSORT_DIR := qsort/
-TEST_BINS += $(BIN_DIR)/qsort
-
-$(BIN_DIR)/qsort: $(QSORT_DIR)/qsort_main.c $(QSORT_DIR)/util.h $(BIN_DIR)
-	riscv64-unknown-linux-gnu-gcc -DRISCV ${CFLAGS} -o $@ $<
-
