6df508a0f5f5ab468446932bea57d13e0fedb21a
Sagar Karandikar <sagark@eecs.berkeley.edu>
2017-11-19 05:03:30 +0000

    l2 interf run

--- buildroot-overlay/bin/mutilatemasterauto.sh
+++ buildroot-overlay/bin/mutilatemasterauto.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # pass in expected QPS as first arg
-mutilate3 -s 192.168.0.2 --noload -T 4 -t 1 -v -i exponential:1 -a 192.168.0.3 -a 192.168.0.4 -a 192.168.0.5 -a 192.168.0.6 -a 192.168.0.7 -a 192.168.0.8 --scan=200:4000:100
+mutilate3 -s 192.168.0.2 --noload -T 4 -v -i exponential:1 -a 192.168.0.3 -a 192.168.0.4 -a 192.168.0.5 -a 192.168.0.6 -a 192.168.0.7 -a 192.168.0.8 --scan=200:4000:100
