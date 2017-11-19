5110bf2b007384538382c0f38f2e0f6dab092cf2
Sagar Karandikar <sagark@eecs.berkeley.edu>
2017-11-19 01:23:35 +0000

    script updates

--- buildroot-overlay/bin/mutilatemaster.sh
+++ buildroot-overlay/bin/mutilatemaster.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # pass in expected QPS as first arg
-mutilate3 -s 192.168.0.2 --noload -T 4 -Q 1000 -D 4 -C 4 -t 1 -q $1 -c 4 -i exponential:1 -a 192.168.0.3 -a 192.168.0.4 -a 192.168.0.5 -a 192.168.0.6 -a 192.168.0.7 -a 192.168.0.8
+mutilate3 -s 192.168.0.2 --noload -T 4 -t 1 -q $1 -i exponential:1 -a 192.168.0.3 -a 192.168.0.4 -a 192.168.0.5 -a 192.168.0.6 -a 192.168.0.7 -a 192.168.0.8
