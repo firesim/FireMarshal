ee88515e19e727c5d54d9188546551046699977c
Nathan Pemberton <nathanp@berkeley.edu>
2017-11-19 18:46:42 -0800

    Test 100% local. Print raw numbers (better for scripting).

--- pfa_tests/full.sh
+++ pfa_tests/full.sh
@@ -9,23 +9,28 @@ cat /sys/kernel/mm/pfa_stat_label >> $LOG
 echo "Running genome tests"
 pushd /root/genome
 
+echo 64M > /sys/fs/cgroup/pfa_cg/memory.max
+echo -n "64000000,Genome," >> $LOG
+mytime pfa_launch ./assemble little.dat 2>> $LOG
+cat /sys/kernel/mm/pfa_stat >> $LOG
+
 echo 48M > /sys/fs/cgroup/pfa_cg/memory.max
-echo -n "48M,Genome," >> $LOG
+echo -n "48000000,Genome," >> $LOG
 mytime pfa_launch ./assemble little.dat 2>> $LOG
 cat /sys/kernel/mm/pfa_stat >> $LOG
 
 echo 32M > /sys/fs/cgroup/pfa_cg/memory.max
-echo -n "32M,Genome," >> $LOG
+echo -n "32000000,Genome," >> $LOG
 mytime pfa_launch ./assemble little.dat 2>> $LOG
 cat /sys/kernel/mm/pfa_stat >> $LOG
 
 echo 24M > /sys/fs/cgroup/pfa_cg/memory.max
-echo -n "24M,Genome," >> $LOG
+echo -n "24000000,Genome," >> $LOG
 mytime pfa_launch ./assemble little.dat 2>> $LOG
 cat /sys/kernel/mm/pfa_stat >> $LOG
 
 echo 16M > /sys/fs/cgroup/pfa_cg/memory.max
-echo -n "16M,Genome," >> $LOG
+echo -n "16000000,Genome," >> $LOG
 mytime pfa_launch ./assemble little.dat 2>> $LOG
 cat /sys/kernel/mm/pfa_stat >> $LOG
 
@@ -34,24 +39,29 @@ popd
 echo "Running qsort tests"
 pushd /root/qsort
 
+echo 64M > /sys/fs/cgroup/pfa_cg/memory.max
+mytime pfa_launch ./qsort 64000000 2>> $LOG
+echo -n "64000000,Qsort," >> $LOG
+cat /sys/kernel/mm/pfa_stat >> $LOG
+
 echo 48M > /sys/fs/cgroup/pfa_cg/memory.max
 mytime pfa_launch ./qsort 64000000 2>> $LOG
-echo -n "48M,Qsort," >> $LOG
+echo -n "48000000,Qsort," >> $LOG
 cat /sys/kernel/mm/pfa_stat >> $LOG
 
 echo 32M > /sys/fs/cgroup/pfa_cg/memory.max
 mytime pfa_launch ./qsort 64000000 2>> $LOG
-echo -n "32M,Qsort," >> $LOG
+echo -n "32000000,Qsort," >> $LOG
 cat /sys/kernel/mm/pfa_stat >> $LOG
 
 echo 24M > /sys/fs/cgroup/pfa_cg/memory.max
 mytime pfa_launch ./qsort 64000000 2>> $LOG
-echo -n "24M,Qsort," >> $LOG
+echo -n "24000000,Qsort," >> $LOG
 cat /sys/kernel/mm/pfa_stat >> $LOG
 
 echo 16M > /sys/fs/cgroup/pfa_cg/memory.max
 mytime pfa_launch ./qsort 64000000 2>> $LOG
-echo -n "16M,Qsort," >> $LOG
+echo -n "16000000,Qsort," >> $LOG
 cat /sys/kernel/mm/pfa_stat >> $LOG
 
 popd
