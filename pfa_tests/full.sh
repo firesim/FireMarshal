c153dc20dd786d5b7c8326c90cbcedb622b70fcd
Nathan Pemberton <nathanp@berkeley.edu>
2017-11-19 21:12:17 -0800

    Run qsort first to get some data before crash? Also re-order Benchmar,MemSize to match analysis scripts

--- pfa_tests/full.sh
+++ pfa_tests/full.sh
@@ -6,62 +6,62 @@ LOG=/root/full_log.csv
 echo -n "Benchmark,MemSize,TotalRuntime," >> $LOG
 cat /sys/kernel/mm/pfa_stat_label >> $LOG
 
-echo "Running genome tests"
-pushd /root/genome
-
-echo 64M > /sys/fs/cgroup/pfa_cg/memory.max
-echo -n "64000000,Genome," >> $LOG
-mytime pfa_launch ./assemble little.dat 2>> $LOG
-cat /sys/kernel/mm/pfa_stat >> $LOG
-
-echo 48M > /sys/fs/cgroup/pfa_cg/memory.max
-echo -n "48000000,Genome," >> $LOG
-mytime pfa_launch ./assemble little.dat 2>> $LOG
-cat /sys/kernel/mm/pfa_stat >> $LOG
-
-echo 32M > /sys/fs/cgroup/pfa_cg/memory.max
-echo -n "32000000,Genome," >> $LOG
-mytime pfa_launch ./assemble little.dat 2>> $LOG
-cat /sys/kernel/mm/pfa_stat >> $LOG
-
-echo 24M > /sys/fs/cgroup/pfa_cg/memory.max
-echo -n "24000000,Genome," >> $LOG
-mytime pfa_launch ./assemble little.dat 2>> $LOG
-cat /sys/kernel/mm/pfa_stat >> $LOG
-
-echo 16M > /sys/fs/cgroup/pfa_cg/memory.max
-echo -n "16000000,Genome," >> $LOG
-mytime pfa_launch ./assemble little.dat 2>> $LOG
-cat /sys/kernel/mm/pfa_stat >> $LOG
-
-popd
-
 echo "Running qsort tests"
 pushd /root/qsort
 
 echo 64M > /sys/fs/cgroup/pfa_cg/memory.max
 mytime pfa_launch ./qsort 64000000 2>> $LOG
-echo -n "64000000,Qsort," >> $LOG
+echo -n "Qsort,64000000," >> $LOG
 cat /sys/kernel/mm/pfa_stat >> $LOG
 
 echo 48M > /sys/fs/cgroup/pfa_cg/memory.max
 mytime pfa_launch ./qsort 64000000 2>> $LOG
-echo -n "48000000,Qsort," >> $LOG
+echo -n "Qsort,48000000," >> $LOG
 cat /sys/kernel/mm/pfa_stat >> $LOG
 
 echo 32M > /sys/fs/cgroup/pfa_cg/memory.max
 mytime pfa_launch ./qsort 64000000 2>> $LOG
-echo -n "32000000,Qsort," >> $LOG
+echo -n "Qsort,32000000," >> $LOG
 cat /sys/kernel/mm/pfa_stat >> $LOG
 
 echo 24M > /sys/fs/cgroup/pfa_cg/memory.max
 mytime pfa_launch ./qsort 64000000 2>> $LOG
-echo -n "24000000,Qsort," >> $LOG
+echo -n "Qsort,24000000," >> $LOG
 cat /sys/kernel/mm/pfa_stat >> $LOG
 
 echo 16M > /sys/fs/cgroup/pfa_cg/memory.max
 mytime pfa_launch ./qsort 64000000 2>> $LOG
-echo -n "16000000,Qsort," >> $LOG
+echo -n "Qsort,16000000," >> $LOG
+cat /sys/kernel/mm/pfa_stat >> $LOG
+
+popd
+
+echo "Running genome tests"
+pushd /root/genome
+
+echo 64M > /sys/fs/cgroup/pfa_cg/memory.max
+echo -n "Genome,64000000," >> $LOG
+mytime pfa_launch ./assemble little.dat 2>> $LOG
+cat /sys/kernel/mm/pfa_stat >> $LOG
+
+echo 48M > /sys/fs/cgroup/pfa_cg/memory.max
+echo -n "Genome,48000000," >> $LOG
+mytime pfa_launch ./assemble little.dat 2>> $LOG
+cat /sys/kernel/mm/pfa_stat >> $LOG
+
+echo 32M > /sys/fs/cgroup/pfa_cg/memory.max
+echo -n "Genome,32000000," >> $LOG
+mytime pfa_launch ./assemble little.dat 2>> $LOG
+cat /sys/kernel/mm/pfa_stat >> $LOG
+
+echo 24M > /sys/fs/cgroup/pfa_cg/memory.max
+echo -n "Genome,24000000," >> $LOG
+mytime pfa_launch ./assemble little.dat 2>> $LOG
+cat /sys/kernel/mm/pfa_stat >> $LOG
+
+echo 16M > /sys/fs/cgroup/pfa_cg/memory.max
+echo -n "Genome,16000000," >> $LOG
+mytime pfa_launch ./assemble little.dat 2>> $LOG
 cat /sys/kernel/mm/pfa_stat >> $LOG
 
 popd
