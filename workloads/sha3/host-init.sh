5b15de41fd10b5eb552dc3b554e6a1d7312a65a1
Albert Ou <aou@eecs.berkeley.edu>
2019-09-28 02:26:55 +0000

    Add SHA3 workload with John the Ripper

---
+++ workloads/sha3/host-init.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -ex
+
+export PATH="${PWD}/../../wlutil/br/buildroot/output/host/bin:${PATH}"
+destdir="${PWD}/overlay"
+
+# NOTE: For linking to work correctly with search paths provided by
+# pkg-config, the buildroot external toolchain must be used (which
+# ensures that the buildroot sysroot is used instead of the original).
+
+cd JohnTheRipper/src
+./configure --host=riscv64-unknown-linux-gnu \
+	--enable-pkg-config \
+	--disable-native-tests \
+	--disable-openmp \
+	--with-systemwide
+make
+
+cd ../run
+install -d "${destdir}/usr/bin" "${destdir}/usr/share/john/rules"
+install -D -m 0755 -t "${destdir}/usr/bin" john
+install -D -m 0644 -t "${destdir}/usr/share/john" *.chr *.conf *.lst
+install -D -m 0644 -t "${destdir}/usr/share/john/rules" rules/*
